2fcec6b8de9836f9209bf535c917abb722494eb5
Utkarsh Sahu <74239122+utkarsh4tech@users.noreply.github.com>
2022-07-24 22:43:19 +0530

    added files for w8 assignment

---
+++ app.py
@@ -0,0 +1,12 @@
+from tkinter import N
+import streamlit as st
+
+st.header("Week 8 Assignment- Utkarsh Sahu- 21f1001107")
+
+number1 = st.number_input('Insert first number',value=0) 
+
+number2 = st.number_input('Insert second number',value=0)
+
+if number1 is not None and number2 is not None:
+    st.write("Sum of the numbers is: {}".format((number1+number2)))
+
